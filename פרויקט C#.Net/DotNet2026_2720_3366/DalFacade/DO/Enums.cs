86f9907e96d2bab43f1336a537eab5e11ec67a8b
sara <sara0527656182@gmail.com>
2025-12-04 01:37:22 +0200

    project

---
+++ פרויקט C#.Net/DotNet2026_2720_3366/DalFacade/DO/Enums.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DalFacade.DO;
+public record Product(int ProductId, string ProductName, Enum ProductCategorey,double Price,int Amount)
+{ }
+ public record Custumer(int CustomerId, string CustomerName,string? Address,string? PhoneNumber)
+{
+
+}
+public record Sale(int SaleId, int ProductId,int RequiredAmount,double SalePrice,bool ForAllCustomers,DateTime BeginSale,DateTime EndSale)
+{
+
+}
